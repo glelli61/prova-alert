6f1ce7c125365f7e7f853efbf4aa44f948bbb300
glelli61 <128994499+glelli61@users.noreply.github.com>
2023-03-26 19:27:53 +0200

    1 file modified

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById("app");
 appDiv.innerHTML = "<h2>Prova con gli alert</h2>";
-window.alert("Primo");
-window.alert("Secondo");
-window.alert("Terzo");
+console.log("Primo");
+console.log("Secondo");
+console.log("Terzo");
